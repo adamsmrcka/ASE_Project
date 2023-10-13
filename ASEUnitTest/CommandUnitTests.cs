4d30241dd0801b81b52e87d4bc1c7d7b3c252983
adamSmrcka <adams@ADAMNTB>
2023-10-13 14:38:13 +0100

    Added Clear function unit test

--- ASEUnitTest/CommandUnitTests.cs
+++ ASEUnitTest/CommandUnitTests.cs
@@ -19,6 +19,45 @@ namespace ASEUnitTest
 
         Form1 form1 = new Form1();
 
+        public bool CompareBitmapsLazy(Bitmap bmp1, Bitmap bmp2)
+        {
+            if (bmp1 == null || bmp2 == null)
+                return false;
+            if (object.Equals(bmp1, bmp2))
+                return true;
+            if (!bmp1.Size.Equals(bmp2.Size) || !bmp1.PixelFormat.Equals(bmp2.PixelFormat))
+                return false;
+
+            //Compare bitmaps using GetPixel method
+            for (int column = 0; column < bmp1.Width; column++)
+            {
+                for (int row = 0; row < bmp1.Height; row++)
+                {
+                    if (!bmp1.GetPixel(column, row).Equals(bmp2.GetPixel(column, row)))
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        public void testHelp(Action<Parser> action, Action<Parser> action1)
+        {
+            Bitmap bitmap1 = new Bitmap(640, 480);
+            Bitmap bitmap2 = new Bitmap(640, 480);
+
+            Graphics graphics1 = Graphics.FromImage(bitmap1);
+            Graphics graphics2 = Graphics.FromImage(bitmap2);
+
+            Canvas canvas = new Canvas(graphics2, form1);
+            parser = Parser.getParser();
+
+            action(parser);
+            action1(parser);
+
+            Assert.AreEqual(true, CompareBitmapsLazy(bitmap1, bitmap2));
+        }
+
         [TestInitialize]
         public void Initialize()
         {
@@ -112,6 +151,44 @@ namespace ASEUnitTest
 
             Canvas.posX = Canvas.defaultPosX;
             Canvas.posY = Canvas.defaultPosY;
+        }
+
+        [TestMethod]
+        public void TestCommands_ShouldDraw_Reset()
+        {
+            Canvas.posX = 50;
+            Canvas.posY = 50;
+            // Arrange
+            string[] lines = { "reset" };
+
+            // Act
+            parser.parseCommand(lines);
+
+            // Assert
+            Assert.AreEqual(Canvas.defaultPosX, Canvas.posX);
+            Assert.AreEqual(Canvas.defaultPosY, Canvas.posY);
+
+        }
+
+        [TestMethod]
+        public void TestCommands_ShouldDraw_Clear()
+        {
+            // Arrange
+            // Create a Graphics object and a Canvas
+            Bitmap bitmap = new Bitmap(800, 600);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            Canvas canvas = new Canvas(graphics, null);
+
+            // Draw something on the canvas
+            canvas.getGraphics().FillRectangle(new SolidBrush(Color.Black), 10, 10, 100, 100);
+
+            // Act
+            canvas.clearCanvas();
+
+            // Assert
+            // After clearing, the canvas should be empty (white background)
+            Color pixelColor = bitmap.GetPixel(10, 10); // Assuming defaultPosX and defaultPosY
+            Assert.AreEqual(ColorTranslator.FromHtml("#FFFFFF"), pixelColor);
         }
     }
 }
